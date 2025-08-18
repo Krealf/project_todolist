d67a20deb41e29d5a6cca662132fc9f2e8adaa37
Konstantin Uchaev <krealf@mail.ru>
2025-08-18 16:23:07 +0500

    Отрисовка полученных значений

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
 // Globals
+const todoList = document.querySelector("#todo-list");
 let todos = [];
 let users = [];
 
 // Attach events
 document.addEventListener("DOMContentLoaded", initApp);
 
-// Event Logic
-function initApp() {
-  Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
-    [todos, users] = values;
-  });
+// Basic logic
+function getUserName(userId) {
+  const user = users.find((el) => el.id === userId);
+  return user.name;
+}
 
-  // Отправить в разметку
+function printTodo({ id, userId, title, completed }) {
+  const li = document.createElement("li");
+  li.classList.add("todo-item");
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} by <b>${getUserName(userId)}</b></span>`;
+
+  const status = document.createElement("input");
+  status.type = "checkbox";
+  status.checked = completed;
+
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.classList.add("close");
+
+  li.prepend(status);
+  li.append(close);
+
+  todoList.prepend(li);
+}
+
+// Event Logic
+async function initApp() {
+  try {
+    [todos, users] = await Promise.all([getAllTodos(), getAllUsers()]);
+
+    todos.forEach((todo) => {
+      printTodo(todo);
+    });
+  } catch (error) {
+    console.error("Error initializing app:", error);
+  }
 }
 
 // Async logic
